ee9b59587628e8c31ab4cc49616d7102a9e20fe8
orest77 <orest.shkhumov@gmail.com>
2018-09-07 13:49:08 +0300

    HW4

---
+++ HW4/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    class Person
+    {
+        private string name;
+        private DateTime birthYear;
+
+        public string Name { get { return name; } }
+        public DateTime BirthYear { get { return birthYear; } }
+
+        public Person()
+        {
+
+        }
+        public Person(string _name, DateTime _birthyear)
+        {
+            this.name = _name;
+            this.birthYear = _birthyear;
+        }
+
+        public int Age()
+        {
+            return DateTime.Now.Year - BirthYear.Year;
+        }
+        public void Input()
+        {
+            Console.Write("Input person name: ");
+            name = Console.ReadLine();
+            Console.Write("Input person birthday (yyyy) : ");
+            birthYear = DateTime.ParseExact(Console.ReadLine(), "yyyy",null);
+
+        }
+
+        public void ChangeName(string n)
+        {
+            this.name = n;
+        }
+        public override string ToString()
+        {
+            return $"{name} + {Age()}".ToString();
+        }
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+        public static bool operator ==(Person first, Person second)
+        {
+            return (first.Name == second.Name);
+        }
+        public static bool operator !=(Person first, Person second)
+        {
+            return !(first == second);
+        }
+    }
+}
---
+++ HW4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person[] person = new Person[3];
+
+            for (int i = 0; i < person.Length; i++)
+            {
+                Person pers = new Person();
+                pers.Input();
+                person[i] = pers;
+            }
+
+            foreach (var pers in person)
+            {
+                if (pers.Age() < 16)
+                {
+                    pers.ChangeName("Very Young");
+                }
+                pers.Output();
+            }
+
+            
+            for (int i = 0; i < person.Length; i++)
+            {
+                for (int a = 0; a < person.Length; a++)
+                {
+                    if (a == i) continue;
+                    if (person[i].Name == person[a].Name)
+                    {                        
+                        Console.Write($"The persons with the same names :");                        
+                        person[i].Output();
+                    }
+                    
+
+                }
+                
+            }
+           
+           
+
+
+            Console.ReadKey();
+        }
+    }
+}
