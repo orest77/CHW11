c623b054b1581d7c7ad903eb57d46b9ddcd701a8
orest77 <orest.shkhumov@gmail.com>
2018-09-18 14:33:50 +0300

    Xml & Json serialization

--- HW4/Person.cs
+++ HW4/Person.cs
@@ -1,23 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+
 
 namespace HW4
 {
-    class Person
+    [Serializable]
+    [DataContract]
+    public class Person
     {
         private string name;
         private DateTime birthYear;
 
-        public string Name { get { return name; } }
-        public DateTime BirthYear { get { return birthYear; } }
+        //[XmlAttribute]
+        [DataMember]
+        public string Name { get { return name; } set { } }
+
+        //[XmlAttribute]
+        [DataMember]
+        public DateTime BirthYear { get { return birthYear; } set { } }
 
         public Person()
         {
-
         }
+        
         public Person(string _name, DateTime _birthyear)
         {
             this.name = _name;
@@ -28,11 +40,12 @@ namespace HW4
         {
             return DateTime.Now.Year - BirthYear.Year;
         }
+
         public void Input()
         {
             Console.Write("Input person name: ");
             name = Console.ReadLine();
-            Console.Write("Input person birthday (yyyy) : ");
+            Console.Write("Input person birthday (YYYY) : ");
             birthYear = DateTime.ParseExact(Console.ReadLine(), "yyyy",null);
 
         }
@@ -41,21 +54,46 @@ namespace HW4
         {
             this.name = n;
         }
+
         public override string ToString()
         {
             return $"{name} + {Age()}".ToString();
         }
+
         public void Output()
         {
             Console.WriteLine(ToString());
         }
+
         public static bool operator ==(Person first, Person second)
         {
-            return (first.Name == second.Name);
+            return (first.name == second.name);
         }
+
         public static bool operator !=(Person first, Person second)
         {
             return !(first == second);
+        }
+
+        public void SaveInXmlFormat(string fileName)
+        {          
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(Person),
+                new Type[] { typeof(Person) });
+            using (FileStream fStream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+            {
+                xmlFormat.Serialize(fStream, this);
+            }
+            Console.WriteLine("--> Save object in XML-format");
+        }
+        public void SaveInJsonFormat(string jsfileName)
+        {
+            DataContractJsonSerializer JsonFormat = new DataContractJsonSerializer(typeof(Person),
+                new Type[] { typeof(Person) });
+            using (FileStream fStream = new FileStream(jsfileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+            {
+                JsonFormat.WriteObject(fStream, this);
+            }
+            Console.WriteLine("--> Save object in JSON-format");
         }
     }
 }
